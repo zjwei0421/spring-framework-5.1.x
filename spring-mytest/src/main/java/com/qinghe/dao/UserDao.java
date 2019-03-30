8bc8afcfa63189421e9d82a11b8939f06a0c84ce
qinghe <qinghe@maihaoche.com>
2019-03-31 00:02:07 +0800

    first commit

---
+++ spring-mytest/src/main/java/com/qinghe/dao/UserDao.java
@@ -0,0 +1,23 @@
+/**
+ * Allrights Reserved,Designed By www.maihaoche.com
+ *
+ * @Package dao
+ * @author 清禾 (qinghe@maihaoche.com)
+ * @date 2019/3/30 11:52 PM
+ * @Copyright 2017-2018 www.maihaoche.comInc.Allrightsreserved.
+ * 注意: 本内容仅限于卖好车内部传阅，禁止外泄以及用于其他的商业项目
+ */
+package com.qinghe.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>  </p> 
+ *
+ * @author 清禾（qinghe@maihaoche.com）
+ * @date 2019/3/30 11:52 PM
+ * @since V1.0
+ */
+public interface UserDao {
+	void query();
+}
